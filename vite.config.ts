d830f56112a408425b972de73dab7c871f183862
KygoSkyrus <kygoskyrus@gmail.com>
2026-08-14 00:54:51 +0530

    fixed deployment issue (vite config)

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,43 @@
-// import { defineConfig, mergeConfig } from 'vite'
-// import { tanstackViteConfig } from '@tanstack/vite-config'
+// // import { defineConfig, mergeConfig } from 'vite'
+// // import { tanstackViteConfig } from '@tanstack/vite-config'
+// // import tailwindcss from '@tailwindcss/vite'
+// // import react from '@vitejs/plugin-react'
+
+// // const config = defineConfig({
+// //   plugins: [
+// //     tailwindcss(),
+// //     react(),
+// //   ],
+// //   base: '/', 
+// // })
+
+// // export default mergeConfig(
+// //   config,
+// //   tanstackViteConfig({
+// //     entry: './src/start.ts',
+// //     srcDir: './src',
+// //   }),
+// // )
+
+
+// import { defineConfig } from 'vite'
+// import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 // import tailwindcss from '@tailwindcss/vite'
 // import react from '@vitejs/plugin-react'
+// import tsconfigPaths from 'vite-tsconfig-paths'
 
-// const config = defineConfig({
+// export default defineConfig({
 //   plugins: [
+//     tsconfigPaths(),
+//     tanstackStart(),
 //     tailwindcss(),
 //     react(),
 //   ],
-//   base: '/', 
+//   base: '/',
 // })
 
-// export default mergeConfig(
-//   config,
-//   tanstackViteConfig({
-//     entry: './src/start.ts',
-//     srcDir: './src',
-//   }),
-// )
-
-
 import { defineConfig } from 'vite'
-import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
@@ -29,7 +45,7 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 export default defineConfig({
   plugins: [
     tsconfigPaths(),
-    tanstackStart(),
+    TanStackRouterVite(),
     tailwindcss(),
     react(),
   ],
