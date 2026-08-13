958d1e176604e414b33ed4f05465d95bb3c00b1a
KygoSkyrus <kygoskyrus@gmail.com>
2026-08-14 00:50:09 +0530

    fixed deployment issue (vite config)

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,37 @@
-import { defineConfig, mergeConfig } from 'vite'
-import { tanstackViteConfig } from '@tanstack/vite-config'
+// import { defineConfig, mergeConfig } from 'vite'
+// import { tanstackViteConfig } from '@tanstack/vite-config'
+// import tailwindcss from '@tailwindcss/vite'
+// import react from '@vitejs/plugin-react'
+
+// const config = defineConfig({
+//   plugins: [
+//     tailwindcss(),
+//     react(),
+//   ],
+//   base: '/', 
+// })
+
+// export default mergeConfig(
+//   config,
+//   tanstackViteConfig({
+//     entry: './src/start.ts',
+//     srcDir: './src',
+//   }),
+// )
+
+
+import { defineConfig } from 'vite'
+import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
-const config = defineConfig({
+export default defineConfig({
   plugins: [
+    tsconfigPaths(),
+    tanstackStart(),
     tailwindcss(),
+    react(),
   ],
+  base: '/',
 })
-
-export default mergeConfig(
-  config,
-  tanstackViteConfig({
-    entry: './src/start.ts',
-    srcDir: './src',
-  }),
-)
